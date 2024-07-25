9993aa18383207cd8e1a3c5b35b0c1955f2e6f59
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2024-07-25 21:15:19 +0630

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -49,32 +49,3 @@ document.addEventListener("DOMContentLoaded", function() {
             noResults.style.display = 'none';
         }
     });
-
-    document.querySelector('.toggle-nav').addEventListener('click', function() {
-        document.querySelector('.menu-list').classList.toggle('active');
-    });
-
-    document.querySelector('.switch').addEventListener('click', function() {
-        var searchBar = document.getElementById('searchBar');
-        var title = document.getElementById('title');
-        var searchIcon = document.querySelector('.search-icon');
-
-        if (searchBar.style.display === 'none' || searchBar.style.display === '') {
-            searchBar.style.display = 'block';
-            title.style.display = 'none';
-            searchIcon.src = 'img/close-white.png';
-        } else {
-            searchBar.style.display = 'none';
-            title.style.display = 'block';
-            searchIcon.src = 'img/search-white.png';
-            searchBar.value = '';
-            // Reset the filter
-            var postContainers = document.querySelectorAll('.post-container');
-            var noResults = document.getElementById('noResults');
-            postContainers.forEach(function(container) {
-                container.style.display = '';
-            });
-            noResults.style.display = 'none';
-        }
-    });
-});
