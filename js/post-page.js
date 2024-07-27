6ebcb3387bdb0036f7a6e8a4326e501831b7da0f
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2024-07-27 17:48:22 +0630

    Add files via upload

--- js/post-page.js
+++ js/post-page.js
@@ -1,3 +1,29 @@
+// Check if URL parameters exist, if not redirect to home page
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const postUrl = urlParams.get('post');
+    
+    if (!postUrl) {
+        window.location.href = '../index.html'; // Redirect to home page if no post parameter
+    } else {
+        fetch('../posts.json')
+        .then(response => response.json())
+        .then(data => {
+            const post = data.find(p => p.purl === postUrl);
+            if (post) {
+                document.getElementById('page-title').textContent = post.ptitle;
+                document.getElementById('post-title').textContent = post.ptitle;
+                document.getElementById('post-content').innerHTML = post.content; // Use innerHTML instead of innerText
+                document.getElementById('post-image').src = post.imgurl;
+                document.getElementById('download-btn').addEventListener('click', function() {
+                    window.open(post.dlink, '_blank');
+                });
+            }
+        })
+        .catch(error => console.error('Error loading the post content:', error));
+    }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     var toggleNav = document.querySelector('.toggle-nav');
     var menuList = document.querySelector('.menu-list');
