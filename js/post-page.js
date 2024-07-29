fd0c48363fc1b94ad64ed7f835598c83dd29daa2
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2024-07-29 11:03:20 +0630

    Update post-page.js

--- js/post-page.js
+++ js/post-page.js
@@ -23,36 +23,3 @@ document.addEventListener("DOMContentLoaded", function() {
         .catch(error => console.error('Error loading the post content:', error));
     }
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    var toggleNav = document.querySelector('.toggle-nav');
-    var menuList = document.querySelector('.menu-list');
-    var switchElement = document.querySelector('.switch');
-    var searchBar = document.getElementById('searchBar');
-    var title = document.getElementById('title');
-    var searchIcon = document.querySelector('.search-icon');
-
-    if (switchElement) {
-        switchElement.addEventListener('click', function() {
-            if (searchBar.style.display === 'none' || searchBar.style.display === '') {
-                searchBar.style.display = 'block';
-                title.style.display = 'none';
-                searchIcon.src = 'img/close-white.png';
-            } else {
-                searchBar.style.display = 'none';
-                title.style.display = 'block';
-                searchIcon.src = 'img/search-white.png';
-                searchBar.value = '';
-                // Reset the filter
-                var postContainers = document.querySelectorAll('.post-container');
-                var noResults = document.getElementById('noResults');
-                postContainers.forEach(function(container) {
-                    container.style.display = '';
-                });
-                if (noResults) {
-                    noResults.style.display = 'none';
-                }
-            }
-        });
-    }
-});
