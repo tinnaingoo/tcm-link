981690e64fb6c87ff97464c783bc937b14f9b7d5
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2024-07-29 11:44:59 +0630

    Update post-page.js

--- js/post-page.js
+++ js/post-page.js
@@ -1,25 +1,29 @@
-// Check if URL parameters exist, if not redirect to home page
-document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const postUrl = urlParams.get('post');
-    
-    if (!postUrl) {
-        window.location.href = '../index.html'; // Redirect to home page if no post parameter
-    } else {
-        fetch('../posts.json')
-        .then(response => response.json())
-        .then(data => {
-            const post = data.find(p => p.purl === postUrl);
-            if (post) {
-                document.getElementById('page-title').textContent = post.ptitle;
-                document.getElementById('post-title').textContent = post.ptitle;
-                document.getElementById('post-content').innerHTML = post.content; // Use innerHTML instead of innerText
-                document.getElementById('post-image').src = post.imgurl;
-                document.getElementById('download-btn').addEventListener('click', function() {
-                    window.open(post.dlink, '_blank');
-                });
-            }
-        })
-        .catch(error => console.error('Error loading the post content:', error));
-    }
-});
+    document.addEventListener("DOMContentLoaded", function() {
+        // Extract query parameter from URL
+        const urlParams = new URLSearchParams(window.location.search);
+        const postUrl = urlParams.get('post');
+
+        if (postUrl) {
+            fetch('posts.json')
+            .then(response => response.json())
+            .then(data => {
+                const post = data.find(p => p.purl === postUrl);
+                if (post) {
+                    document.getElementById('page-title').textContent = post.ptitle;
+                    document.getElementById('post-title').textContent = post.ptitle;
+                    document.getElementById('post-image').src = post.imgurl;
+                    document.getElementById('post-content').textContent = post.content;
+                    document.getElementById('lince').textContent = post.lince;
+                    document.getElementById('os').textContent = post.os;
+                    document.getElementById('developer').textContent = post.dev;
+                    document.getElementById('date').textContent = post.date;
+                    document.getElementById('fsize').textContent = post.fsize;
+                } else {
+                    document.getElementById('post-title').textContent = 'Post not found';
+                }
+            })
+            .catch(error => console.error('Error loading the post:', error));
+        } else {
+            document.getElementById('post-title').textContent = 'No post specified';
+        }
+    });
