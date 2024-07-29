d6da127a3656ce19e83423c2b49376a8e9ae2632
Tin Naing Oo <54192779+tinnaingoo@users.noreply.github.com>
2024-07-29 10:32:38 +0630

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             postContainer.className = 'post-container';
             
             postContainer.innerHTML = `
+                <div class="row">
                 <div class="col-12 col-s-12 col-m-6 col-l-6 col-xl-6">
                 <div class="image-container">
                         <img src="${post.imgurl}" alt="Sample Image">
@@ -22,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     <div class="p-title"><a>${post.ptitle}</a></div>
                     <div class="p-description"><a>${post.prescription}</a></div>
                     <div class="p-next"><a href="post.html?post=${post.purl}">Read More &#8811;</a></div>
+                </div>
                 </div>
                 </div>
             `;
